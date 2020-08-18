de95760ebbc6ae164f78b92c3f11f29ce0483b96
Uttam <uttamo.sharma@gmail.com>
2020-08-18 20:19:00 +0100

    Added a dropdown for rating with the 10 possible values, rather than having a generic integer field

--- books/forms.py
+++ books/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Select
 from .models import Review
 
 
@@ -6,3 +6,22 @@ class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = ['rating', 'text']
+        # (value, label) pairs
+        widgets = {
+            'rating': Select(choices=([
+                (1, '1 ★'),
+                (2, '2 ★★'),
+                (3, '3 ★★★'),
+                (4, '4 ★★★★'),
+                (5, '5 ★★★★★'),
+                (6, '6 ★★★★★★'),
+                (7, '7 ★★★★★★★'),
+                (8, '8 ★★★★★★★★'),
+                (9, '9 ★★★★★★★★★'),
+                (10, '10 ★★★★★★★★★★')
+            ])),
+        }
+
+        labels = {
+            'text': 'Review',
+        }
